c74cc8b674f1a4da1670e5a8bc4206c0f71df903
mickmetalholic <yuanshenmick@gmail.com>
2024-07-05 02:34:56 +0800

    Update index.ts

--- scripts/comics-converter/index.ts
+++ scripts/comics-converter/index.ts
@@ -1,23 +1,26 @@
-import { resolve, extname, basename } from 'node:path';
+import { resolve, extname } from 'node:path';
 import fs from 'fs-extra';
 import AdmZip from 'adm-zip';
 
-const tempFolder = resolve(__dirname, '.temp');
-fs.ensureDirSync(tempFolder);
-fs.emptydirSync(tempFolder);
-
 const DIR_PATH = resolve(__dirname, './archives');
-const books = fs.readdirSync(DIR_PATH);
-const zipFiles = books.reduce((res, book) => {
-  const folderPath = resolve(DIR_PATH, book);
-  const files = fs.readdirSync(folderPath)
-    .filter(file => extname(file) === '.zip')
-    .map(file => resolve(folderPath, file));
-  res.push(...files);
-  return res;
-}, [] as string[]);
+const serieses = fs.readdirSync(DIR_PATH);
+serieses.forEach((series) => {
+  const books = fs.readdirSync(resolve(DIR_PATH, series));
+  books.forEach((book) => {
+    const folderPath = resolve(DIR_PATH, series, book);
+    const files = fs.readdirSync(folderPath)
+      .filter(file => extname(file) === '.zip')
+      .map(file => resolve(folderPath, file));
+    const file = files[0];
+    if (file) {
+      fs.ensureDirSync(resolve(__dirname, 'result', series));
+      processZipFile(file, series, `${book}.zip`);
+    }
+  });
+  fs.removeSync(resolve(DIR_PATH, series));
+});
 
-function processZipFile(file: string) {
+function processZipFile(file: string, series: string, book: string) {
   const zip = new AdmZip(file);
   const newZip = new AdmZip();
 
@@ -29,11 +32,5 @@ function processZipFile(file: string) {
     }
   });
 
-  newZip.writeZip(resolve(__dirname, 'result', basename(file)));
-}
-
-// processZipFile(zipFiles[0]);
-
-for (const file of zipFiles) {
-  processZipFile(file);
+  newZip.writeZip(resolve(__dirname, 'result', series, book));
 }
